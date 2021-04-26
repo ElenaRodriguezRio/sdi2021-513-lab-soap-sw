87c27ee5a4f2882219150cb436c8008801d6c073
Elena <UO264329@uniovi.es>
2021-04-27 01:25:32 +0200

    first commit

---
+++ sdi2021-513-lab-soap-sw/src/test/java/com/example/demo/Sdi2021513LabSoapSwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2021513LabSoapSwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
